ca4d5754b38826566dca8f85d053063c84c2a36b
AMPADamiaMateu <webampadamiamateu@gmail.com>
2017-11-25 19:03:48 +0100

    update web

---
+++ js/jsampaaccordion_v5.js
@@ -0,0 +1,9 @@
+(function($) { 
+alert(1);
+	jQuery.getScript('https://agora.xtec.cat/ceipdamiamateu/wp-includes/js/jquery/ui/accordion.min.js');
+
+	jQuery(document).ready(function($)
+	{  
+alert(2);	
+	});
+})( jQuery );
